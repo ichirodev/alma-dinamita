c781df241c331d0babab4bd12b705c014f236637
Adan Schiffer <adanschiffer@hotmail.com>
2022-01-08 02:36:39 -0600

    Player: Fixed an error when sliding
    Sliding now ends faster 'cause it doesn't hit the SlideToPosition z axis.

--- alma-dinamita/Assets/Player/slide.cs
+++ alma-dinamita/Assets/Player/slide.cs
@@ -20,23 +20,20 @@ public class slide : MonoBehaviour
     }
     private void Update()
     {
-        if (fixedSlideToPosition == transform.position || EndSlide())
+        if (fixedSlideToPosition == transform.position || EndSlideByPlayerOrWorld())
         {
-            isSliding = false;
-            CharControllerComponent.enabled = true;
-            MovComponent.enabled = true;
-            fixedSlideToPosition = new Vector3(0, 0, 0);
+            CancelSlide();
         }
         if (CharControllerComponent.isGrounded && MovComponent.sprinting && Input.GetKeyDown(KeyCode.LeftControl) && !isSliding)
         {
-            isSliding = true;
-            fixedSlideToPosition = slideToPosition.position;
-            CharControllerComponent.enabled = false;
-            MovComponent.enabled = false;
+            StartSlide();
         }
         if (isSliding)
         {
+            // Slide the player
             transform.position = Vector3.Lerp(transform.position, fixedSlideToPosition, slideSpeed * Time.deltaTime);
+            // Check if the player still grounded, if not then stop sliding and return the control
+            // of the player to the Character Controller
             groundCheckRay = new Ray(slideGroundCheck.transform.position, -Vector3.up);
             if (Physics.Raycast(groundCheckRay, out rayHitOnGround))
             {
@@ -44,10 +41,7 @@ public class slide : MonoBehaviour
                 {
                     if (rayHitOnGround.distance > maxHeightWhenSliding)
                     {
-                        isSliding = false;
-                        CharControllerComponent.enabled = true;
-                        MovComponent.enabled = true;
-                        fixedSlideToPosition = new Vector3(0, 0, 0);
+                        CancelSlide();
                         Debug.Log(rayHitOnGround.distance);
                     }
                 }
@@ -55,21 +49,45 @@ public class slide : MonoBehaviour
         }
     }
 
+    void CancelSlide()
+    {
+        isSliding = false;
+        CharControllerComponent.enabled = true;
+        MovComponent.enabled = true;
+        fixedSlideToPosition = new Vector3(0, 0, 0);
+    }
+
+    void StartSlide()
+    {
+        isSliding = true;
+        fixedSlideToPosition = slideToPosition.position;
+        CharControllerComponent.enabled = false;
+        MovComponent.enabled = false;
+    }
+
     // The slide action could end for various reasons
     // 1. Object collition: if the controller collition detects a wall the slide ends
     // 2. Slide cancel: Allow the player to take control over the controller before
     // the slide ends
-    private bool EndSlide()
+    private bool EndSlideByPlayerOrWorld()
     {
-        float canSlideCancel = fixedSlideToPosition.z - 0.26f;
-        // Slide cancel
-        if (transform.position.z >= canSlideCancel)
+        float canSlideCancelZPoint = fixedSlideToPosition.z - 0.26f;
+        float forceSlideToEndZPoint = fixedSlideToPosition.z - 0.02001f;
+        // Slide cancel, the player can end the slide before it gets slow by jumping
+        if (transform.position.z >= canSlideCancelZPoint)
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 return true;
             }
         }
+        // Cancel slide automatically, due to the use of Lerp, there is a moment where the player gets
+        // horribly slow when sliding so the slide has to finish before that moment
+        if (transform.position.z >= forceSlideToEndZPoint)
+        {
+            return true;
+        }
+
         return false;
     }
 }
